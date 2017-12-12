c562193190242b51bff64978518366cc86e60108
Alexey <alexcas@mail.ru>
2017-12-12 20:41:57 +0200

    v 20

--- app/src/main/java/com/accherniakocich/android/freecourier/Activityes/Chat/Chat.java
+++ app/src/main/java/com/accherniakocich/android/freecourier/Activityes/Chat/Chat.java
@@ -1,6 +1,7 @@
 package com.accherniakocich.android.freecourier.Activityes.Chat;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +12,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.accherniakocich.android.freecourier.Activityes.StartActivity;
@@ -30,6 +32,10 @@ public class Chat extends AppCompatActivity {
     private ImageButton image_button_send_message_chat;
     private EditText edit_text_chat;
     private ArrayList<String>listMessage;
+    private TextView all_couriers,check_couriers;
+    private ArrayList <String> listCouriersCheck;
+    private boolean selectCouriers = false;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,26 +46,62 @@ public class Chat extends AppCompatActivity {
     }
 
     private void init() {
+        listCouriersCheck = new ArrayList<>();
         Intent intent = getIntent();
         String fromWhere = intent.getStringExtra("fromWhere");
         edit_text_chat = (EditText)findViewById(R.id.edit_text_chat);
         image_button_send_message_chat = (ImageButton)findViewById(R.id.image_button_send_message_chat);
         if (fromWhere.equals("courier")){
             ((TableRow) findViewById(R.id.chat_admin_container)).setVisibility(View.INVISIBLE);
+            ((TableRow) findViewById(R.id.chat_admin_container_check_couriers)).setVisibility(View.INVISIBLE);
         }
         listMessage = new ArrayList<>();
 
         list_chat = (ListView)findViewById(R.id.list_chat);
 
+        all_couriers = (TextView)findViewById(R.id.all_couriers);
+        check_couriers = (TextView)findViewById(R.id.check_couriers);
+        all_couriers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                all_couriers.setBackgroundColor(Color.GREEN);
+            }
+        });
+        check_couriers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Chat.this,ListCouriersForChat.class);
+                startActivityForResult(i,9);
+                check_couriers.setBackgroundColor(Color.GREEN);
+            }
+        });
+
         image_button_send_message_chat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!edit_text_chat.getText().toString().equals("")){
+
+                if (!edit_text_chat.getText().toString().equals("")
+                        &&!selectCouriers){
                     long dateMessage = new Date().getTime();
                     FirebaseDatabase.getInstance().getReference().child("chat").child(dateMessage+"")
                             .setValue(edit_text_chat.getText().toString());
                     edit_text_chat.setText("");
+
                     Toast.makeText(Chat.this, "Сообщение отправлено", Toast.LENGTH_SHORT).show();
+                }else if (!edit_text_chat.getText().toString().equals("")
+                        &&selectCouriers){
+                    if (selectCouriers&&!listCouriersCheck.isEmpty()){
+                        if (!edit_text_chat.getText().toString().equals("")){
+                            for (int i = 0;i<listCouriersCheck.size();i++){
+                                long dateMessage = new Date().getTime();
+                                FirebaseDatabase.getInstance().getReference().child("chatForSelectCouriers")
+                                        .child(listCouriersCheck.get(i))
+                                        .setValue(edit_text_chat.getText().toString());
+                            }
+                            Toast.makeText(Chat.this, "Сообщение будет отправлено!", Toast.LENGTH_SHORT).show();
+                            edit_text_chat.setText("");
+                        }
+                    }
                 }
             }
         });
@@ -70,7 +112,7 @@ public class Chat extends AppCompatActivity {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 listMessage.add(dataSnapshot.getValue(String.class));
-                ArrayAdapter adapter = new ArrayAdapter(Chat.this,android.R.layout.simple_list_item_1,listMessage);
+                adapter = new ArrayAdapter(Chat.this,android.R.layout.simple_list_item_1,listMessage);
                 list_chat.setAdapter(adapter);
             }
 
@@ -94,5 +136,42 @@ public class Chat extends AppCompatActivity {
 
             }
         });
+        FirebaseDatabase.getInstance().getReference().child("chatForSelectCouriers").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                listMessage.add(dataSnapshot.getValue(String.class));
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {return;}
+        if (requestCode==9){
+            Log.d(StartActivity.LOG_TAG,"swich = " + selectCouriers);
+            selectCouriers = data.getBooleanExtra("swich",false);
+            listCouriersCheck = data.getStringArrayListExtra("list");
+        }
     }
 }
---
+++ app/src/main/java/com/accherniakocich/android/freecourier/Activityes/Chat/ListCouriersForChat.java
@@ -0,0 +1,82 @@
+package com.accherniakocich.android.freecourier.Activityes.Chat;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.accherniakocich.android.freecourier.Activityes.StartActivity;
+import com.accherniakocich.android.freecourier.R;
+import com.accherniakocich.android.freecourier.Сlasses.Courier;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class ListCouriersForChat extends AppCompatActivity {
+
+    private ListView list_with_couriers_chat;
+    private ArrayList <String> listCouriers;
+    private ArrayList <String>list;
+    private Button button_ok_couriers_chat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_couriers_for_chat);
+        init();
+    }
+
+    private void init() {
+        button_ok_couriers_chat = (Button)findViewById(R.id.button_ok_couriers_chat);
+        list = new ArrayList<>();
+        listCouriers = new ArrayList<>();
+        list_with_couriers_chat = (ListView)findViewById(R.id.list_with_couriers_chat);
+        formatedList();
+        list_with_couriers_chat.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                listCouriers.add(list.get(position));
+                view.setBackgroundColor(Color.GREEN);
+            }
+        });
+
+        button_ok_couriers_chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("list", listCouriers);
+                intent.putExtra("swich", true);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+
+    private void formatedList() {
+        FirebaseDatabase.getInstance().getReference().child("couriers").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot data: dataSnapshot.getChildren()) {
+                    list.add(data.getValue(Courier.class).getNameCourier());
+                }
+                ArrayAdapter adapter = new ArrayAdapter(ListCouriersForChat.this,android.R.layout.simple_list_item_1,list);
+                list_with_couriers_chat.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
